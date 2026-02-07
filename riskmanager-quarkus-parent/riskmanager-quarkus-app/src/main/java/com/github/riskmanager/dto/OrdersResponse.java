ddfa6edf699a6f4e449a22c4d449fb67f3339d77
Alexander Kerner <alexkerner1000@gmail.com>
2026-02-07 14:37:56 +0200

    Clean up code

--- riskmanager-quarkus-parent/riskmanager-quarkus-app/src/main/java/com/github/riskmanager/dto/OrdersResponse.java
+++
@@ -1,9 +0,0 @@
-package com.github.riskmanager.dto;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import java.util.List;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public record OrdersResponse(
-        List<Order> orders
-) {}
